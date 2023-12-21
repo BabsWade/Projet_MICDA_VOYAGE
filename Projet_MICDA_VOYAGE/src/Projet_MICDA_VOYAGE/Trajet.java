7966e2a418c3e09986dd24910c103a6fb54e13bc
ndiayemnsour <zocod3r@gmail.com>
2023-12-21 20:35:37 +0000

    la fonction Ajour

--- Projet_MICDA_VOYAGE/src/Projet_MICDA_VOYAGE/Trajet.java
+++ Projet_MICDA_VOYAGE/src/Projet_MICDA_VOYAGE/Trajet.java
@@ -1,20 +1,28 @@
 package Projet_MICDA_VOYAGE;
-
+import  Projet_MICDA_VOYAGE.ConnexionBD;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.text.DateFormatter;
 
 import com.toedter.calendar.JDateChooser;
 import com.toedter.components.JLocaleChooser;
 import com.toedter.components.JLocaleChooserBeanInfo;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -24,23 +32,26 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.AbstractListModel;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JSpinner;
 import javax.swing.JFormattedTextField;
+import javax.swing.ListSelectionModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Trajet extends JFrame {
+	private ConnexionBD connexionDB;
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtCodeTrajet;
 	private JTextField txtNomTrajet;
-	private JTextField txtHeureDepart;
 	private JTable tableTrajet;
 	private JDateChooser dateDepart;
-	private JLocaleChooser heureDepart;
-
 	/**
 	 * Launch the application.
 	 */
@@ -61,13 +72,14 @@ public class Trajet extends JFrame {
 	 * Create the frame.
 	 */
 	public Trajet() {
+		//connexionDB = new ConnexionDB();
 		setTitle("Trajet Bus");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1500, 800);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.decode("#04224c"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
@@ -141,11 +153,18 @@ public class Trajet extends JFrame {
 		btnUpdate.setBounds(411, 254, 117, 35);
 		contentPane.add(btnUpdate);
 		
+		
+		//Bouton Ajout
+		
 		JButton btnAdd = new JButton("Add");
+		
 		btnAdd.setForeground(UIManager.getColor("CheckBox.foreground"));
 		btnAdd.setFont(new Font("Lucida Grande", Font.BOLD, 16));
 		btnAdd.setBounds(570, 255, 117, 35);
 		contentPane.add(btnAdd);
+		
+		
+		
 		
 		JButton btnDelete = new JButton("Delete");
 		btnDelete.setForeground(UIManager.getColor("CheckBox.foreground"));
@@ -154,18 +173,45 @@ public class Trajet extends JFrame {
 		contentPane.add(btnDelete);
 		
 		//Tableau		
-        tableTrajet = new JTable();
+  
+        
+    // Créer un DefaultTableModel avec les noms de colonnes
+		DefaultTableModel model = new DefaultTableModel();
+        tableTrajet = new JTable(model);
+		model.addColumn("CodeBus");
+        model.addColumn("NomBus");
+        model.addColumn("Description");
+        model.addColumn("NombreSiege");
+        model.addColumn("EtatBus");
+        model.addColumn("Action");
+        // Ajouter la table avec le modèle
+        
+        tableTrajet.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		tableTrajet.setSurrendersFocusOnKeystroke(true);
 		tableTrajet.setFillsViewportHeight(true);
 		tableTrajet.setColumnSelectionAllowed(true);
 		tableTrajet.setCellSelectionEnabled(true);
 		tableTrajet.setBounds(8, 320, 1420, 420);
 		contentPane.add(tableTrajet);
+        tableTrajet = new JTable(model);
+        //fillTable();
+        JScrollPane scrollPane = new JScrollPane(tableTrajet);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+        // Ajouter la table au contentPane
+        tableTrajet.setBounds(8, 320, 1420, 420);
+        getContentPane().add(new JScrollPane(tableTrajet)); // Utilisation de JScrollPane pour gérer les en-têtes de colonnes
+
+
+        // Ajouter la table au contentPane
+        tableTrajet.setBounds(8, 320, 1420, 420);
+        getContentPane().add(new JScrollPane(tableTrajet)); // Utilisation de JScrollPane pour gérer les en-têtes de colonnes
 		
 		JComboBox comboBoxVilleArrivee = new JComboBox();
 		comboBoxVilleArrivee.setModel(new DefaultComboBoxModel(new String[] {"Dakar", "Kaolack", "Fatick", "Thies", "Faffrine", "Tambacounda", "Kedougou", "Sédhiou", "Louga", "Saint-Louis", "Diourbel", "Kolda", "Ziguinchor", "Matam"}));
 		comboBoxVilleArrivee.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
 		comboBoxVilleArrivee.setBounds(500, 173, 200, 30);
+		 getContentPane().add(new JScrollPane(tableTrajet)); // Utilisation de JScrollPane pour gérer les en-têtes de colonnes
 		contentPane.add(comboBoxVilleArrivee);
 		
 		JComboBox comboBoxBusAssocie = new JComboBox();
@@ -184,13 +230,75 @@ public class Trajet extends JFrame {
 		//Date depart
 		dateDepart = new JDateChooser();
 		dateDepart.setBounds(850, 165, 200, 30);
-		add(dateDepart);
+		getContentPane().add(dateDepart);
+		
 		
 		//heureDepart
 		SimpleDateFormat heureDepart = new SimpleDateFormat("HH:MM");//Creer un objet SimpleDateFormat pour formatter l'heure si on formatte pas il affiche par defaut date et heure
 		JFormattedTextField txtHeureDepart = new JFormattedTextField(new DateFormatter(heureDepart));//Creer JFormattedTextField avec le DateFormatter
 		txtHeureDepart.setBounds(1220, 96, 200, 30);
 		txtHeureDepart.setValue(new Date()); // valeur par defaut date actuel
-		add(txtHeureDepart);
+		getContentPane().add(txtHeureDepart);
+		
+		//Fonction Ajout dans la BD
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				// Recuperation des donnees du formualaires
+				 String codeTrajet = txtCodeTrajet.getText();
+				 String nomTrajet = txtNomTrajet.getText();
+				 String Depart = comboBoxVilleDepart.getSelectedItem().toString();
+				 String Arrivee = comboBoxVilleArrivee.getSelectedItem().toString();
+				 String Bus = comboBoxBusAssocie.getSelectedItem().toString();
+				// Récupérer la date de départ
+			    Date date = dateDepart.getDate();
+			    java.sql.Date dateSql = new java.sql.Date(date.getTime());
+			 // Récupérer l'heure de départ
+			    Date heure = (Date) txtHeureDepart.getValue();
+			    java.sql.Time heureSql = new java.sql.Time(heure.getTime());
+			
+			 // Connexion à la base de données
+			    try (Connection connection = connexionDB.getConnection()) {
+		            // Utilisez la connexion pour vos opérations sur la base de données
+			    	 // Requête SQL pour l'insertion des données
+			        String query = "INSERT INTO Trajet (codeTrajet, busAssocie, nomTrajet, villeDepart, villeArrivee, dateDepart, heureDepart ) VALUES (?, ?, ?, ?, ?, ?, ?)";
+			        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+			            // Définir les valeurs des paramètres
+			            preparedStatement.setString(1, codeTrajet);
+			            preparedStatement.setString(3, nomTrajet);
+			            preparedStatement.setString(4, Depart);
+			            preparedStatement.setString(5, Arrivee);
+			            preparedStatement.setDate(6, dateSql);
+			            preparedStatement.setTime(7, heureSql);
+			            preparedStatement.setString(2, Bus);
+
+			            // Exécuter la requête
+			            int rowsAffected = preparedStatement.executeUpdate();
+
+			            // Vérifier si l'insertion a réussi
+			            if (rowsAffected > 0) {
+			            	JOptionPane.showMessageDialog(null, "Données ajoutées avec succès", null, 0);
+			             			                
+			                /*// Mettre à jour le modèle de la table
+			                DefaultTableModel model = (DefaultTableModel) tableTrajet.getModel();
+			                model.addRow(new Object[]{codeTrajet, nomTrajet, villeDepart, villeArrivee, dateSql, heureSql, });*/
+			            } else {
+			            	JOptionPane.showMessageDialog(null, "Échec de l'ajout des données", null, 0);
+			            }
+			        }
+		        } catch (SQLException e) {
+		            e.printStackTrace();
+		            JOptionPane.showMessageDialog(null, "Erreur de connexion à la base de données", null, 0);
+		        }
+			    
+			    System.out.println(" Code Trajet :"+codeTrajet+"\n Bus Associe : "+Bus+"\n Nom Trajet :"+nomTrajet+"\n Ville Depart :"+Depart+"\n Ville Arrivee :"+Arrivee+"\n Date Depart :"+dateSql+"\n Heure Depart :"+heureSql);
+			}
+
+			
+		});
+		
+		
+		
+	
+		
 	}
 }
